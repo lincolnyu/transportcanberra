3d71e02bdc75ae503be8c2f3ce1ae12819c1c3dc
quanb <quanb@QUANBEN-ASUS>
2016-09-03 18:25:56 +1000

    capability to tell where the user is

---
+++ TransportCanberra/TransportCanberra/GeoServices/GeolocationService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Windows.Devices.Geolocation;
+
+namespace TransportCanberra.GeoServices
+{
+    public class GeolocationService
+    {
+        public enum PositionUpdateReasons
+        {
+            Normal,
+            Disabled,
+            AccessDenied,
+            AccessUnspecified
+        }
+
+        public delegate void StatusChangedEventHandler(Geolocator sender, StatusChangedEventArgs args);
+
+        public delegate void PositionUpdatedEventHandler(Geoposition position, PositionUpdateReasons reason);
+
+        public GeolocationService(uint reportIntervalMilliseconds = 1000, uint? desiredAccuracyInMeters = null)
+        {
+            DesireAccuracyInMetersValue = desiredAccuracyInMeters;
+            ReportIntervalMilliseconds = reportIntervalMilliseconds;
+        }
+
+        public uint? DesireAccuracyInMetersValue { get; }
+        
+        public uint ReportIntervalMilliseconds { get; }
+
+        public event StatusChangedEventHandler StatusChanged;
+
+        public event PositionUpdatedEventHandler PositionUpdated;
+
+        public async void Initialize()
+        {
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            switch (accessStatus)
+            {
+                case GeolocationAccessStatus.Allowed:
+
+                    // If DesiredAccuracy or DesiredAccuracyInMeters are not set (or value is 0), DesiredAccuracy.Default is used.
+                    var geolocator = new Geolocator
+                    {
+                        DesiredAccuracyInMeters = DesireAccuracyInMetersValue,
+                        ReportInterval = ReportIntervalMilliseconds
+                    };
+
+                    // Subscribe to the StatusChanged event to get updates of location status changes.
+                    geolocator.StatusChanged += OnStatusChanged;
+                    geolocator.PositionChanged += OnPositionChanged;
+
+                    // Carry out the operation.
+                    Geoposition pos = await geolocator.GetGeopositionAsync();
+
+                    PositionUpdated?.Invoke(pos, PositionUpdateReasons.Normal);
+
+                    break;
+                case GeolocationAccessStatus.Denied:
+                    PositionUpdated?.Invoke(null, PositionUpdateReasons.AccessDenied);
+                    break;
+                case GeolocationAccessStatus.Unspecified:
+                    PositionUpdated?.Invoke(null, PositionUpdateReasons.AccessUnspecified);
+                    break;
+            }
+        }
+
+        private void OnPositionChanged(Geolocator sender, PositionChangedEventArgs args)
+        {
+            PositionUpdated?.Invoke(args.Position, PositionUpdateReasons.Normal);
+        }
+
+        private void OnStatusChanged(Geolocator sender, StatusChangedEventArgs args)
+        {
+            if (args.Status == PositionStatus.Disabled)
+            {
+                PositionUpdated?.Invoke(null, PositionUpdateReasons.AccessDenied);
+            }
+
+            StatusChanged?.Invoke(sender, args);
+        }
+    }
+}
---
+++ TransportCanberra/TransportCanberra/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using TransportCanberra.GeoServices;
+using Windows.UI.Xaml.Controls.Maps;
+using Windows.Foundation;
+using Windows.Storage.Streams;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TransportCanberra
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MapIcon _locationPin;
+        private GeolocationService _geolocation;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (_geolocation == null)
+            {
+                _geolocation = new GeolocationService();
+                _geolocation.PositionUpdated += GeolocationPositionUpdated;
+                _geolocation.Initialize();
+            }
+        }
+
+        private async void GeolocationPositionUpdated(Windows.Devices.Geolocation.Geoposition position, GeolocationService.PositionUpdateReasons reason)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,
+                async () =>
+                {
+                    if (position != null)
+                    {
+                        await CanberraMap.TrySetViewAsync(position.Coordinate.Point, 18);
+                        if (_locationPin == null)
+                        {
+                            _locationPin = new MapIcon();
+                            _locationPin.Image = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/me.png"));
+                            //  _locationPin.NormalizedAnchorPoint = new Point(0.5, 1.0);
+                            _locationPin.ZIndex = 0;
+                            CanberraMap.MapElements.Add(_locationPin);
+                        }
+                        _locationPin.Location = position.Coordinate.Point;
+                    }
+                });
+        }
+    }
+}
